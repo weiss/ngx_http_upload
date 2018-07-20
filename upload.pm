482653aa5c7f8af2e83f6c68bb15ac9d898a00cd
Holger Weiss <holger@zedat.fu-berlin.de>
2018-07-20 23:33:28 +0200

    Copy license terms back into upload.pm file
    The license is short enough.

--- upload.pm
+++ upload.pm
@@ -1,12 +1,19 @@
 # Nginx module to handle file uploads and downloads for ejabberd's
 # mod_http_upload or Prosody's mod_http_upload_external.
 
-# --------------------------------------------------------------------
 # Copyright (c) 2018 Holger Weiss <holger@zedat.fu-berlin.de>
 #
-# This module is published under the terms of the ISC license, see the
-# LICENSE.txt file.
-# --------------------------------------------------------------------
+# Permission to use, copy, modify, and/or distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+# REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+# AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+# INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+# LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+# OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+# PERFORMANCE OF THIS SOFTWARE.
 
 package upload;
 
